3bd77bf4781076da7163e84be87fdec1b45df8bb
Padraig Littlefield <108550814+padraiglittlefield@users.noreply.github.com>
2023-06-08 12:35:11 -0400

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -12,11 +12,7 @@ def main():
     weights = {"Age": 1, "Gls": 1, "Ast": 1, "G+A": 1, "xG" : 1, "xAG": 1, "PrgC": 1, "PrgP": 1}
     
     team = str(input("Enter a Football Team: "))
-    #team = "Brentford"
-    #team_ranking = int(team_stats[team]["Ranking"])
-    #average_team = average_stats(team_stats, team_ranking - 5, team_ranking + 2)
     
-    #player_score(player_stats, weights)
     top_rated = player_score(player_stats, att_weight(team_stats[team], team_stats))
     for i in range(5):
         print("{}: {}".format(i + 1, top_rated[i]))
